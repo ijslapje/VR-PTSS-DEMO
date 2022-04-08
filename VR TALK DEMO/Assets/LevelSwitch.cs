7f48710c1a19055924d00ffe313779cd039b19d3
Ian Robeerst <ianrobeerst@livewallgroup.com>
2022-04-08 11:19:17 +0200

    Add project

---
+++ VR TALK DEMO/Assets/LevelSwitch.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSwitch : MonoBehaviour
+{
+    public int currLevel = 0;
+    public string[] levelNames = new string[2] { "Menu", "DesertWorld" };
+
+
+    static LevelSwitch s = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(s == null)
+        {
+            s = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void Startgame()
+    {
+        currLevel = (currLevel + 1) % 2;
+        Valve.VR.SteamVR_LoadLevel.Begin(levelNames[currLevel]);
+        DestroyScriptInstance();
+    }
+
+    void DestroyScriptInstance()
+    {
+        // Removes this script instance from the game object
+        Destroy(this);
+    }
+
+}
---
+++ VR TALK DEMO/Assets/SceneWithNestedLayoutElementsLoadScript.cs
@@ -0,0 +1,12 @@
+
+using UnityEngine;
+
+public class SceneWithNestedLayoutElementsLoadScript : MonoBehaviour
+{
+    public bool isStartCalled { get; private set; }
+
+    protected void Start()
+    {
+        isStartCalled = true;
+    }
+}
